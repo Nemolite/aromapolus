e5dffb2e26e5292779809e85bd29de802e7f26d4
Nemolite <g25092011@mail.ru>
2023-03-21 07:52:37 +0300

    start

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * aromapolus - Дочерняя тема для темы envo-store
+ * 
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Helper
+ */
+function show($param){
+	echo "<pre>";
+	print_r($param);
+	echo "</per>";
+}
