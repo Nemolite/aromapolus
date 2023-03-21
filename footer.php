bbd5f66919cb6a4f49ecbfffbebf95d3b9a38af1
Nemolite <g25092011@mail.ru>
2023-03-21 10:00:40 +0300

    init

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer id="colophon" class="footer-credits">
+	<div class="text-center">
+		<?php printf( __( 'Proudly powered by %s', 'envo-store' ), '<a href="' . esc_url( __( 'https://wordpress.org/', 'envo-store' ) ) . '">WordPress</a>' ); ?>
+		<span class="sep"> | </span>
+		<?php printf( __( 'Theme: %1$s by %2$s', 'envo-store' ), '<a href="http://envothemes.com/envo-store/">Envo Store</a>', 'EnvoThemes' ); ?>
+		<span class="sep"> | </span>
+		<?php printf( __( '%s - Разработка web-проектов', 'envo-store' ), '<a href="' . esc_url( __( 'http://vandraren.ru/', 'envo-store' ) ) . '">VANDRAREN</a>' ); ?>
+	</div>      
+</footer> 
+<!-- end main container -->
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -14,3 +14,15 @@ function show($param){
 	print_r($param);
 	echo "</per>";
 }
+
+/**
+ * Подключение scipts and styles
+ */ 
+function aromapolus_scripts_style() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'aromapolus-style', get_stylesheet_directory_uri() .'/assets/css/aromapolus.css' );
+	wp_enqueue_script( 'aromapolus-script', get_stylesheet_directory_uri() . '/assets/js/aromapolus.js', array(), $theme_version, true );
+}
+add_action( 'wp_enqueue_scripts', 'aromapolus_scripts_style' );
